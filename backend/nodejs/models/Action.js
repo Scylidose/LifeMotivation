5295c14ab35e52c2f1b006416cf6d872caf0196b
Scylidose <jordisaleilles@hotmail.fr>
2023-08-16 08:47:24 -0400

    Can now create and fetch actions.

--- backend/nodejs/models/Action.js
+++ backend/nodejs/models/Action.js
@@ -15,15 +15,19 @@ class Action {
   }
 
   static create(title, description, author, isGood, importance, frequency, difficulty, intendedDuration) {
+    console.log("INSERTING : ", title, description, author, isGood, importance, frequency, difficulty, 0, intendedDuration);
     return new Promise((resolve, reject) => {
       db.run(
-        'INSERT INTO actions (title, description, author, isGood, importance, frequency, difficulty, intendedDuration) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-        [title, description, author, isGood, importance, frequency, difficulty, intendedDuration],
+        'INSERT INTO actions (title, description, author, isGood, importance, frequency, difficulty, consistencyStreak, intendedDuration) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [title, description, author, isGood, importance, frequency, difficulty, 0, intendedDuration],
         function (err) {
           if (err) {
+            console.log("ROLLBACK");
+            db.run('ROLLBACK'); // Roll back the transaction if there's an error
             reject(err);
           } else {
             resolve(new Action(this.lastID, title, description, author, isGood, importance, frequency, difficulty, 0, intendedDuration));
+            console.log(`Row updated: ${this.changes}`);
           }
         }
       );
@@ -31,23 +35,13 @@ class Action {
   }
 
   static findAllByAuthor(author) {
+    console.log("FETCHING....");
     return new Promise((resolve, reject) => {
       db.all('SELECT * FROM actions WHERE author = ?', [author], (err, rows) => {
         if (err) {
-          reject(err);
+          throw err;
         } else {
-          const actions = rows.map(row => new Action(
-            row.id,
-            row.title,
-            row.description,
-            row.author,
-            row.isGood,
-            row.importance,
-            row.frequency,
-            row.difficulty,
-            row.consistencyStreak,
-            row.intendedDuration
-          ));
+          const actions = rows.map(row => new Action(row.id, row.title, row.description, row.author, row.isGood, row.importance, row.frequency, row.difficulty, row.consistencyStreak, row.intendedDuration));
           resolve(actions);
         }
       });
