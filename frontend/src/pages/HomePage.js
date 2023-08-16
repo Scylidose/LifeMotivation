d164d66284ba80e68a6e84a493281d4a13680211
Scylidose <jordisaleilles@hotmail.fr>
2023-08-16 19:10:36 -0400

    Added function to delete action.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,39 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import ActionForm from '../components/ActionForm';
-import { getActionsForUser } from '../services/api';
+import ActionCard from '../components/ActionCard';
+import { getActionsForUser, deleteAction } from '../services/api';
 
 const HomePage = () => {
-    const [actions, setActions] = useState([]);
+  const [actions, setActions] = useState([]);
 
-    useEffect(() => {
-        const fetchActions = async () => {
-          try {
-            const username = 'root'; // To change
-            const actionsData = await getActionsForUser(username);
-            setActions(actionsData);
-            console.log(actionsData);
-          } catch (error) {
-            console.error('Error fetching actions:', error);
-          }
-        };
-    
-        fetchActions();
-      }, []);
+  useEffect(() => {
+    const fetchActions = async () => {
+      try {
+        const username = 'root'; // To change
+        const actionsData = await getActionsForUser(username);
+        setActions(actionsData);
+        console.log(actionsData);
+      } catch (error) {
+        console.error('Error fetching actions:', error);
+      }
+    };
 
-    return (
-        <div>
-            <ActionForm />
-            <h1>Actions</h1>
-            <ul>
-                {actions.map(action => (
-                    <li key={action.id}>
-                        <h2>{action.title}</h2>
-                        <p>{action.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    fetchActions();
+  }, []);
+
+  const handleDelete = async (actionId) => {
+    try {
+      await deleteAction(actionId);
+      setActions(actions.filter((action) => action.id !== actionId));
+    } catch (error) {
+      console.error('Error deleting action:', error);
+    }
+  };
+
+  return (
+    <div>
+      <ActionForm />
+      <h1>Your Habits</h1>
+      <div className="action-list">
+        {actions.map(action => (
+          <ActionCard key={action.id} action={action} onDelete={handleDelete}/>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
