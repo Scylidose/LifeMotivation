f498ff1f71496006b1c66d53f5395c52dd398c66
Scylidose <jordisaleilles@hotmail.fr>
2023-08-16 08:47:01 -0400

    Create actions table if not exists.

--- backend/nodejs/config/dbconfig.js
+++ backend/nodejs/config/dbconfig.js
@@ -3,4 +3,21 @@ const path = require('path');
 
 // Create and connect to the SQLite database
 const db = new sqlite3.Database(path.join(__dirname, '../../database/abitmotivation.db'));
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS actions (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    author TEXT,
+    isGood INTEGER,
+    importance INTEGER,
+    frequency INTEGER,
+    difficulty INTEGER,
+    consistencyStreak INTEGER,
+    intendedDuration INTEGER
+  )
+`);
+
+
 module.exports = db;
