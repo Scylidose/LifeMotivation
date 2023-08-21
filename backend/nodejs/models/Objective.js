77bcb9258fcdfe970e7b850354d50c8f40063d5f
Scylidose <jordisaleilles@hotmail.fr>
2023-08-20 22:42:13 -0400

    Added realFinishDateTime attribute in the returned objective.

--- backend/nodejs/models/Objective.js
+++ backend/nodejs/models/Objective.js
@@ -44,6 +44,32 @@ class Objective {
         });
     }
 
+    static findById(id) {
+        console.log("FETCHING OBJECTIVE BY ID....");
+        return new Promise((resolve, reject) => {
+            db.get('SELECT * FROM objectives WHERE id = ?', [id], (err, row) => {
+                if (err) {
+                    reject(err);
+                } else if (!row) {
+                    resolve(null);
+                } else {
+                    const objective = new Objective(
+                        row.id,
+                        row.title,
+                        row.description,
+                        row.author,
+                        row.priority,
+                        row.complexity,
+                        row.intendedFinishDateTime,
+                        row.publishedDateTime,
+                        row.realFinishDateTime
+                    );
+                    resolve(objective);
+                }
+            });
+        });
+    }
+
     static findAllByAuthor(author) {
         console.log("FETCHING OBJECTIVE FOR ", author);
         return new Promise((resolve, reject) => {
@@ -59,7 +85,8 @@ class Objective {
                         row.priority,
                         row.complexity,
                         row.intendedFinishDateTime,
-                        row.publishedDateTime
+                        row.publishedDateTime,
+                        row.realFinishDateTime
                     ));
                     resolve(objective);
                 }
@@ -67,66 +94,66 @@ class Objective {
         });
     }
 
-  static finishObjectiveById(id) {
-    console.log("UPDATING FINISHED OBJECTIVE:", id);
-    return new Promise((resolve, reject) => {
-      const realFinishDateTime = new Date().getTime();
-      db.run(
-        'UPDATE objectives SET realFinishDateTime = ? WHERE id = ?',
-        [realFinishDateTime, id],
-        async function (err) {
-          if (err) {
-            console.error('Error updating finished objective:', err);
-            reject(err);
-          } else {
-            console.log(`Finished objective updated: ${this.changes}`);
-            const objective = await Objective.findById(id);
-            resolve(objective);
-          }
-        }
-      );
-    });
-  }
+    static finishObjectiveById(id) {
+        console.log("UPDATING FINISHED OBJECTIVE:", id);
+        return new Promise((resolve, reject) => {
+            const realFinishDateTime = new Date().getTime();
+            db.run(
+                'UPDATE objectives SET realFinishDateTime = ? WHERE id = ?',
+                [realFinishDateTime, id],
+                async function (err) {
+                    if (err) {
+                        console.error('Error updating finished objective:', err);
+                        reject(err);
+                    } else {
+                        console.log(`Finished objective updated: ${this.changes}`);
+                        const objective = await Objective.findById(id);
+                        resolve(objective);
+                    }
+                }
+            );
+        });
+    }
 
-  static resetObjectiveById(id) {
-    console.log("UPDATING RESETTING OBJECTIVE:", id);
-    return new Promise((resolve, reject) => {
+    static resetObjectiveById(id) {
+        console.log("UPDATING RESETTING OBJECTIVE:", id);
+        return new Promise((resolve, reject) => {
 
-      db.run(
-        'UPDATE objectives SET realFinishDateTime = ? WHERE id = ?',
-        [null, id],
-        async function (err) {
-          if (err) {
-            console.error('Error updating resetting objective:', err);
-            reject(err);
-          } else {
-            console.log(`Resetting objective updated: ${this.changes}`);
-            const objective = await Objective.findById(id);
-            resolve(objective);
-          }
-        }
-      );
-    });
-  }
+            db.run(
+                'UPDATE objectives SET realFinishDateTime = ? WHERE id = ?',
+                [null, id],
+                async function (err) {
+                    if (err) {
+                        console.error('Error updating resetting objective:', err);
+                        reject(err);
+                    } else {
+                        console.log(`Resetting objective updated: ${this.changes}`);
+                        const objective = await Objective.findById(id);
+                        resolve(objective);
+                    }
+                }
+            );
+        });
+    }
 
-  static deleteById(id) {
-    console.log("DELETING OBJECTIVE:", id);
-    return new Promise((resolve, reject) => {
-      db.run(
-        'DELETE FROM objectives WHERE id = ?',
-        [id],
-        function (err) {
-          if (err) {
-            console.error('Error deleting objective:', err);
-            reject(err);
-          } else {
-            console.log(`Objective deleted: ${this.changes}`);
-            resolve();
-          }
-        }
-      );
-    });
-  }
+    static deleteById(id) {
+        console.log("DELETING OBJECTIVE:", id);
+        return new Promise((resolve, reject) => {
+            db.run(
+                'DELETE FROM objectives WHERE id = ?',
+                [id],
+                function (err) {
+                    if (err) {
+                        console.error('Error deleting objective:', err);
+                        reject(err);
+                    } else {
+                        console.log(`Objective deleted: ${this.changes}`);
+                        resolve();
+                    }
+                }
+            );
+        });
+    }
 }
 
 module.exports = Objective;
