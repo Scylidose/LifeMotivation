557911b27c66820ed7b3dfe832f3e8b67cdeeb02
Scylidose <jordisaleilles@hotmail.fr>
2023-08-22 07:41:52 -0400

    Added comment to describe the functions.

--- frontend/src/components/DotSlider.js
+++ frontend/src/components/DotSlider.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+/**
+ * DotSlider component displays a set of radio buttons with labels.
+ * @param {object} props - The component's properties.
+ * @param {Array} props.labelValues - An array of labels to display.
+ * @param {string} props.type - The type of the slider (e.g., "rating").
+ * @returns {JSX.Element} The DotSlider component.
+ */
 const DotSlider = ({ labelValues, type }) => {
-    const [_selectedValue, setSelectedValue] = useState(null);
 
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
+    // Returns the label for a given value.
+    const getLabel = (value) => {
+        return value;
     };
 
     return (
@@ -17,21 +24,18 @@ const DotSlider = ({ labelValues, type }) => {
                         id={`${type}-${index + 1}`}
                         value={index + 1}
                         required
-                        onChange={handleChange}
                     />
                     {index === 0 || index === labelValues.length - 1 ? (
+                        // Display labels for the first and last items
                         <label htmlFor={`${type}-${index + 1}`} className="slider-label">{getLabel(value)}</label>
                     ) : (
+                        // Empty label for other items
                         <label htmlFor={`${type}-${index + 1}`} className="slider-label"></label>
                     )}
                 </React.Fragment>
             ))}
         </div>
     );
-};
-
-const getLabel = (value) => {
-    return value;
 };
 
 export default DotSlider;
