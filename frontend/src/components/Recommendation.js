d716f8362ae0d2ed1fc7ccd7e9f698534b36c7e9
Scylidose <jordisaleilles@hotmail.fr>
2023-09-08 08:49:57 -0400

    Show recommended action to add in current date.

--- frontend/src/components/Recommendation.js
+++ frontend/src/components/Recommendation.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { actionsApi } from '../services/api/index';
 
-const Recommendation = () => {
-    return(
+const Recommendation = ({ actions }) => {
+
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return (
         <div>
             <p>Recommended actions :</p>
             <ul>
-                
+                {actions.map((action) => {
+                    const currentDayName = daysOfWeek[new Date().getDay()];
+
+                    if (JSON.parse(action.daysOfWeek)[currentDayName.toLowerCase()] || action.frequency == 1 || action.frequency == 2) {
+                        return (
+                            <li key={action.id}>
+                               {action.title}
+                            </li>
+                        );
+                    }
+                    return null;
+                })}
             </ul>
+            <p>test</p>
         </div>
     );
 };
