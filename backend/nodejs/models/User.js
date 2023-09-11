c80a0ebe5eb506fd4e8472a4d6731f3ac8f6362c
Scylidose <jordisaleilles@hotmail.fr>
2023-09-11 18:32:12 -0400

    Added xp attribute.

--- backend/nodejs/models/User.js
+++ backend/nodejs/models/User.js
@@ -1,8 +1,9 @@
 const { db } = require('../config/dbconfig');
 
 class User {
-  constructor(username) {
+  constructor(username, xp) {
     this.username = username;
+    this.xp = xp;
   }
 
   static findOrCreate(username) {
@@ -13,17 +14,36 @@ class User {
           reject(err);
         } else if (!row) {
           console.log("INSERTING NEW USER: ", username);
-          db.run('INSERT INTO users (username) VALUES (?)', [username], function (err) {
+          db.run('INSERT INTO users (username, xp) VALUES (?, 0)', [username], function (err) {
             if (err) {
               reject(err);
             } else {
-              resolve(new User(username));
+              resolve(new User(username, 0));
             }
           });
         } else {
-          resolve(new User(row.username));
+          resolve(new User(row.username, row.xp));
         }
       });
+    });
+  }
+
+  static saveUserXP(username, xp) {
+    console.log("UPDATING USER ", (username) ," EXPERIENCE: ", xp);
+    return new Promise((resolve, reject) => {
+      db.run(
+        'UPDATE users SET xp = ? WHERE username = ?',
+        [xp, username],
+        function (err) {
+          if (err) {
+            console.error('Error updating User XP:', err);
+            reject(err);
+          } else {
+            console.log(`User XP updated: ${this.changes}`);
+            resolve(this.changes);
+          }
+        }
+      );
     });
   }
 
