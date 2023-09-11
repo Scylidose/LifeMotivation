a7e4ba4ea0cd5aa62ce1804d62a5d8e1abe8543b
Scylidose <jordisaleilles@hotmail.fr>
2023-09-11 18:32:01 -0400

    Added route to update xp of an user.

--- backend/nodejs/routes/userRoutes.js
+++ backend/nodejs/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/:username', userController.fetchCreateUser);
+router.post('/:username/:xp', userController.updateUserXP);
 
 module.exports = router;
