e5079a7dccd89f2d6aaf2def5e87950628719e59
Scylidose <jordisaleilles@hotmail.fr>
2023-09-12 18:47:13 -0400

    Added middleware auth token in certain routes.

--- backend/nodejs/routes/userRoutes.js
+++ backend/nodejs/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.get('/:username', userController.fetchCreateUser);
-router.post('/:username/:xp', userController.updateUserXP);
+router.get('/:username', authenticateToken, userController.fetchCreateUser);
+
+router.post('/login', userController.logUser);
+router.post('/register', userController.registerUser);
+
+router.post('/:username/:xp', authenticateToken, userController.updateUserXP);
 
 module.exports = router;
