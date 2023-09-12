eedcad5c6380a0e5056782ab8014279cc8bf1c7a
Scylidose <jordisaleilles@hotmail.fr>
2023-09-12 07:50:24 -0400

    Added basic login component.

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { usersApi } from '../services/api/index';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    usernameOrEmail: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        await usersApi.logUser(formData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="usernameOrEmail"
+          placeholder="Username or Email"
+          value={formData.usernameOrEmail}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
