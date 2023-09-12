98be7a16acc133c60a2e8fbdf5b538c8e904b416
Scylidose <jordisaleilles@hotmail.fr>
2023-09-12 07:58:54 -0400

    Added button to register if user doesn't have an account.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,51 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
+
 import { usersApi } from '../services/api/index';
 
 function Login() {
-  const [formData, setFormData] = useState({
-    usernameOrEmail: '',
-    password: '',
-  });
+    const { user } = useUser();
+    const history = useHistory();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        usernameOrEmail: '',
+        password: '',
     });
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-        await usersApi.logUser(formData);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="usernameOrEmail"
-          placeholder="Username or Email"
-          value={formData.usernameOrEmail}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (user) {
+                // Redirect to the home page if user already logged in
+                history.push('/');
+            } else {
+                const response = await usersApi.logUser(formData);
+                if (response.status === 200) {
+                    // Login successful, redirect to the home page
+                    history.push('/');
+                }
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    useEffect(() => {
+        // If the user is already logged in, redirect to the home page
+        if (user) {
+            history.push('/');
+        }
+    }, [user, history]);
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="usernameOrEmail"
+                    placeholder="Username or Email"
+                    value={formData.usernameOrEmail}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                Don't have an account?{' '}
+                <button onClick={() => history.push('/register')}>
+                    Register here
+                </button>
+            </p>
+        </div>
+    );
 }
 
 export default Login;
