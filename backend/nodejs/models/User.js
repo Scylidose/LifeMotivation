ed7afb301841c54b00ec06c254678e2fff78bb22
Scylidose <jordisaleilles@hotmail.fr>
2023-09-12 18:46:21 -0400

    Added fetch user function and added the new columns in the model.

--- backend/nodejs/models/User.js
+++ backend/nodejs/models/User.js
@@ -1,12 +1,27 @@
 const { db } = require('../config/dbconfig');
+const jwt = require('jsonwebtoken');
+const secretKey = 'your-secret-key';
 
 class User {
-  constructor(username, xp) {
+  constructor(username, xp, email) {
     this.username = username;
     this.xp = xp;
+    this.email = email;
   }
 
-  static findOrCreate(username) {
+  static fetchOne(usernameOrEmail) {
+    return new Promise((resolve, reject) => {
+      db.get("SELECT * FROM users WHERE username = ? OR email = ?", [usernameOrEmail, usernameOrEmail], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row || null);
+        }
+      });
+    })
+  }
+
+  static findOrCreate(username, email = '', password = '', salt = '') {
     return new Promise((resolve, reject) => {
       console.log("CHECK IF USER ", username, " EXISTS");
       db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
@@ -14,22 +29,28 @@ class User {
           reject(err);
         } else if (!row) {
           console.log("INSERTING NEW USER: ", username);
-          db.run('INSERT INTO users (username, xp) VALUES (?, 0)', [username], function (err) {
+          db.run('INSERT INTO users (username, email, password_hash, salt, xp) VALUES (?, ?, ?, ?, 0)', [username, email, password, salt], function (err) {
             if (err) {
               reject(err);
             } else {
-              resolve(new User(username, 0));
+
+              const payload = {
+                userId: this.lastID,
+                username: username,
+              };
+              const token = jwt.sign(payload, secretKey, { expiresIn: '1h', algorithm: 'HS256' });
+              resolve(token);
             }
           });
         } else {
-          resolve(new User(row.username, row.xp));
+          resolve(new User(row.username, row.xp, row.email));
         }
       });
     });
   }
 
   static saveUserXP(username, xp) {
-    console.log("UPDATING USER ", (username) ," EXPERIENCE: ", xp);
+    console.log("UPDATING USER ", (username), " EXPERIENCE: ", xp);
     return new Promise((resolve, reject) => {
       db.run(
         'UPDATE users SET xp = ? WHERE username = ?',
