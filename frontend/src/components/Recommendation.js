b8ce098ed959f1bd5bd6ab0652f0814ee9eb439a
Scylidose <jordisaleilles@hotmail.fr>
2023-09-16 07:18:29 -0400

    Added button to display or hide the recommended actions.

--- frontend/src/components/Recommendation.js
+++ frontend/src/components/Recommendation.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActionForm from './ActionForm';
 
 const Recommendation = ({ actions, currentDate, token }) => {
 
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const [showRecommendedActions, setShowRecommendedActions] = useState(false);
+
+    const toggleRecommendedActions = () => {
+        setShowRecommendedActions(!showRecommendedActions);
+    };
 
     return (
-        <div>
+        <div className="recommendation-container">
             <p>Recommended actions :</p>
-            <ul>
-                {actions.map((action) => {
-                    const currentDayName = daysOfWeek[currentDate.getDay()];
+            <button className="show-actions-button" onClick={toggleRecommendedActions}>
+                {showRecommendedActions ? 'Hide Actions' : 'Show Actions'}
+            </button>
+            {showRecommendedActions && (
+                <ul className="action-list">
+                    {actions.map((action) => {
+                        const currentDayName = daysOfWeek[currentDate.getDay()];
 
-                    if (JSON.parse(action.daysOfWeek)[currentDayName.toLowerCase()] || action.frequency === 1 || action.frequency === 2) {
-                        return (
-                            <li key={action.id}>
-                                {action.title}
-                                <ActionForm
-                                    token={token}
-                                    title={action.title}
-                                    description={action.description}
-                                    isGood={action.isGood}
-                                    daysOfWeek={JSON.parse(action.daysOfWeek)}
-                                    frequency={action.frequency}
-                                    difficulty={action.difficulty}
-                                    intendedDuration={action.intendedDuration}
-                                    selectedObjective={action.objectiveId}
-                                    publishedDateTime={currentDate.getTime()} />
-                            </li>
-                        );
-                    }
-                    return null;
-                })}
-            </ul>
+                        if (JSON.parse(action.daysOfWeek)[currentDayName.toLowerCase()] || action.frequency === 1 || action.frequency === 2) {
+                            return (
+                                <li key={action.id}>
+                                    {action.title}
+                                    <ActionForm
+                                        token={token}
+                                        title={action.title}
+                                        description={action.description}
+                                        isGood={action.isGood}
+                                        daysOfWeek={JSON.parse(action.daysOfWeek)}
+                                        frequency={action.frequency}
+                                        difficulty={action.difficulty}
+                                        intendedDuration={action.intendedDuration}
+                                        selectedObjective={action.objectiveId}
+                                        publishedDateTime={currentDate.getTime()} />
+                                </li>
+                            );
+                        }
+                        return null;
+                    })}
+                </ul>
+            )}
         </div>
     );
 };
